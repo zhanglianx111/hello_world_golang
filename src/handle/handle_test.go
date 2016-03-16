53b11c0349c41b20fd0dae24da546ec5156fd77e
zhanglianx111 <zhanglianx111@aliyun.com>
2016-03-16 14:10:24 +0800

    add handle_test.g

---
+++ src/handle/handle_test.go
@@ -0,0 +1,16 @@
+package handle
+import (
+	"fmt"
+	"testing"
+)
+func Test_HandleDefault(t *testing.T) {
+	fmt.Println("Test_HandleDefault")
+}
+
+func Test_HandleHi(t *testing.T) {
+	fmt.Println("Test_HandleHi")
+}
+
+func Test_HandleName(t *testing.T) {
+	fmt.Println("Test_HandleName")
+}
